59467d6a48bdca9bfdeb4f8ff6c9e024665244fa
vgupta038 <vgupta038@gmail.com>
2022-10-29 13:18:03 +0530

    second commit

---
+++ jwt-Token-Generator-Validator/src/main/java/com/jwt/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.jwt.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@EnableWebSecurity
+@SuppressWarnings("deprecation")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		// We don't need CSRF for this example
+		httpSecurity.csrf().disable()
+				// dont authenticate this particular request
+				.authorizeRequests().antMatchers("/generateToken").permitAll().
+				// all other requests need to be authenticated
+				anyRequest().authenticated().and()
+				.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+				.and()
+				// make sure we use stateless session; session won't be used to
+				// store user's state.
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	
+		// Add a filter to validate the tokens with every request
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+}
---
+++ jwt-Token-Generator-Validator/src/test/java/com/jwt/JwtTokenGeneratorValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenGeneratorValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
